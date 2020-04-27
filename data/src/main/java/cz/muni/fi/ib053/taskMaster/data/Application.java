dbb0c2beb4f18a609e05469a578af8a84adc3ae7
adambananka <addame121@gmail.com>
2020-04-27 20:10:51 +0200

    spring application and database setup

---
+++ data/src/main/java/cz/muni/fi/ib053/taskMaster/data/Application.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.ib053.taskMaster.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application starting point, contains main method.
+ *
+ * @author Adam Baňanka
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
