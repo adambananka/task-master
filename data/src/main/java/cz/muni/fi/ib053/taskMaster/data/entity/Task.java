d1aabe1da953c3aa2e5708454cf30337cc70bf70
Vojtech Kovarik <xkovari7@fi.muni.cz>
2020-04-29 18:24:34 +0200

    fixed comments

--- data/src/main/java/cz/muni/fi/ib053/taskMaster/data/entity/Task.java
+++ data/src/main/java/cz/muni/fi/ib053/taskMaster/data/entity/Task.java
@@ -3,101 +3,119 @@ package cz.muni.fi.ib053.taskMaster.data.entity;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Min;
 
-
-@Entity
+/**
+ * Represents task entity.
+ *
+ * @author Vojtěch Kovářík
+ */
 @Table(name = "tasks")
 public class Task {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
 
-    private User user;
+  @ManyToOne
+  private User user;
 
-    @NotNull
-    private int numInQueue;
+  @Min(0)
+  private int numInQueue;
 
-    @NotNull
-    private int solveTime;
+  @Min(0)
+  private int solveTime;
 
-    @ManyToMany
-    @JoinTable(name="tbl_prerequisites",
-            joinColumns=@JoinColumn(name="taskId"),
-            inverseJoinColumns=@JoinColumn(name="prerequisiteId")
-    )
-    private List<User> prerequisites;
+  @ManyToMany
+  @JoinTable(name = "prerequisites", joinColumns = @JoinColumn(name="taskId", referencedColumnName = "id"),
+          inverseJoinColumns = @JoinColumn(name = "prerequisiteId", referencedColumnName = "id") )
+  private List<Task> prerequisites;
 
-    @ManyToMany
-    @JoinTable(name="tbl_prerequisites",
-            joinColumns=@JoinColumn(name="prerequisiteId"),
-            inverseJoinColumns=@JoinColumn(name="taskId")
-    )
-    private List<User> prerequisiteOf;
+  @ManyToMany(mappedBy = "prerequisites")
+  private List<Task> prerequisiteOf;
 
-    public int getId() { return id; }
+  public int getId() { return id; }
 
-    public void setId(int id) { this.id = id; }
+  public void setId(int id) { this.id = id; }
 
-    public User getUser() { return user; }
+  public User getUser() { return user; }
 
-    public void setUser(User user) { this.user = user; }
+  public void setUser(User user) { this.user = user; }
 
-    public int getNumInQueue() { return numInQueue; }
+  public int getNumInQueue() { return numInQueue; }
 
-    public void setNumInQueue(int numInQueue) { this.numInQueue = numInQueue; }
+  public void setNumInQueue(int numInQueue) { this.numInQueue = numInQueue; }
 
-    public int getSolveTime() { return solveTime; }
+  public int getSolveTime() {
+    return solveTime;
+  }
 
-    public void setSolveTime(int solveTime) { this.solveTime = solveTime; }
+  public void setSolveTime(int solveTime) { this.solveTime = solveTime; }
 
-    public List<User> getPrerequisites() { return prerequisites; }
+  public List<Task> getPrerequisites() { return prerequisites; }
 
-    public void setPrerequisites(List<User> prerequisites) { this.prerequisites = prerequisites; }
+  public void setPrerequisites(List<Task> prerequisites) {
+    this.prerequisites = prerequisites;
+  }
 
-    public List<User> getPrerequisiteOf() { return prerequisiteOf; }
+  public List<Task> getPrerequisiteOf() {
+    return prerequisiteOf;
+  }
 
-    public void setPrerequisiteOf(List<User> prerequisiteOf) { this.prerequisiteOf = prerequisiteOf; }
+  public void setPrerequisiteOf(List<Task> prerequisiteOf) {
+    this.prerequisiteOf = prerequisiteOf;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null) {
-            return false;
-        }
-        if (!(o instanceof Task)) {
-            return false;
-        }
-
-        Task task = (Task) o;
-        return Objects.equals(id, task.getId());
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    if (!(o instanceof Task)) {
+      return false;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+    Task task = (Task) o;
+    if (!Objects.equals(user, task.getUser())) return false;
+    if (numInQueue != task.getNumInQueue()) return false;
+    if (solveTime != task.getSolveTime()) return false;
+    if (!Objects.equals(prerequisites, task.getPrerequisites())) return false;
+    return Objects.equals(prerequisiteOf, task.getPrerequisiteOf());
+  }
 
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", user=" + user +
-                ", numInQueue=" + numInQueue +
-                ", solveTime=" + solveTime +
-                ", prerequisites=" + prerequisites +
-                ", prerequisiteOf=" + prerequisiteOf +
-                '}';
-    }
+  @Override
+  public int hashCode() {
+    int prime = 37;
+    int result = 1;
+
+    result = prime * result + Objects.hashCode(user);
+    result = prime * result + numInQueue;
+    result = prime * result + solveTime;
+    result = prime * result + Objects.hashCode(prerequisites);
+    result = prime * result + Objects.hashCode(prerequisiteOf);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "Task{" +
+            "id=" + id +
+            ", user=" + user +
+            ", numInQueue=" + numInQueue +
+            ", solveTime=" + solveTime +
+            ", prerequisites=" + prerequisites +
+            ", prerequisiteOf=" + prerequisiteOf +
+            '}';
+  }
 }
